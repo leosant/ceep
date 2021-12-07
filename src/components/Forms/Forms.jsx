415ad8456e44e35ac6f1715decb84a265dc6657c
leosant <leonardosantos45548@gmail.com>
2021-12-06 23:37:21 -0300

    alter forms

--- src/components/Forms/Forms.jsx
+++ src/components/Forms/Forms.jsx
@@ -1,23 +1,38 @@
 import React, { Component } from "react";
 import "./style.css";
+
 class Form extends Component {
+  
   render() {
+
     return (
       <form className="forms-registration">
         <input 
          type="text"
          placeholder="Título"
-         className="forms-registration_input" />
+         className="forms-registration_input"
+         onChange={this.handleChangeTitle.bind(this)} />
         <textarea 
          rows={15}
          placeholder="Escreva sua nota..."
          className="forms-registration_input"
-        />
+         />
         <button className="form-registration_input form-registration_submit" >
           Criar nota
         </button>
       </form>
     );
+
+  }
+
+  constructor() {
+    super();
+    this.title = "";
+  }
+  
+  handleChangeTitle(event) {
+    this.title = event.target.value;
+    console.log(this.title);
   }
 }
 
